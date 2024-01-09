29544997bc389439480ee5fb14f5d28e64ab5ec3
domenicolugubre <mimmolg99@gmail.com>
2024-01-09 16:07:27 +0100

    update

--- static/profile.js
+++ static/profile.js
@@ -1,10 +1,13 @@
-
+   // Add an event listener for when the DOM content is loaded
     document.addEventListener('DOMContentLoaded', function () {
-        // Imposta un timeout per nascondere l'elemento dopo un certo periodo
+         // Set a timeout to hide the element after a certain period
         setTimeout(function () {
+            // Select the element with the class 'notify'
             var notifyElement = document.querySelector('.notify');
+             // Check if the element exists
             if (notifyElement) {
+                 // Set the display style to 'none' to hide the element
                 notifyElement.style.display = 'none';
             }
-        }, 1000); // Imposta il tempo in millisecondi (5 secondi nel caso di questo esempio)
+        }, 1000); // Set the time in milliseconds (5 seconds in this example)
     });
--- static/serviceworker.js
+++ static/serviceworker.js
@@ -1,44 +1,53 @@
-// Name of the cache
+
+// Cache name
 let cacheName = 'pwa01';
 
-// List of files to cache
+// List of files to be stored in the cache
 let filesToCache = [
-    'flaskTwitter/templates/login.html',
-    'flaskTwitter/templates/layout.html',
-    'flaskTwitter/templates/register.html',
-    'flaskTwitter/templates/timeline.html',
-    'flaskTwitter/templates/profile.html',
-    'flaskTwitter/templates/followers.html',
-    'flaskTwitter/templates/following.html',
-    'flaskTwitter/templates/hashtag_tweets.html',
-    'flaskTwitter/templates/user_profile.html',
-    'flaskTwitter/static/hashtag_tweets.css',
-    'flaskTwitter/static/like.js',
-    'flaskTwitter/static/login.css',
-    'flaskTwitter/static/profile.css',
-    'flaskTwitter/static/profile.js',
-    'flaskTwitter/static/register.css',
-    'flaskTwitter/static/timeline.css',
-    'flaskTwitter/static/user_profile.css'
+    'twitterProject/templates/login.html',
+    'twitterProject/templates/layout.html',
+    'twitterProject/templates/register.html',
+    'twitterProject/templates/timeline.html',
+    'twitterProject/templates/profile.html',
+    'twitterProject/templates/followers.html',
+    'twitterProject/templates/following.html',
+    'twitterProject/templates/hashtag_tweets.html',
+    'twitterProject/templates/user_profile.html',
+    'twitterProject/static/hashtag_tweets.css',
+    'twitterProject/static/like.js',
+    'twitterProject/static/login.css',
+    'twitterProject/static/profile.css',
+    'twitterProject/static/profile.js',
+    'twitterProject/static/register.css',
+    'twitterProject/static/timeline.css',
+    'twitterProject/static/user_profile.css'
 ];
 
+// Event listener for service worker installation
 self.addEventListener('install', function (e) {
+    // Wait until the cache is opened and files are added to it
         e.waitUntil(
             caches.open(cacheName).then(function (cache) {
                 return cache.addAll(filesToCache);
             }).catch(function (error) {
+                 // Log an error message if installation fails
                 console.error('Error during service worker installation:', error);
             })
         );
 });
 
+// Event listener for service worker activation
 self.addEventListener('activate', function () {
+    // Log a success message upon successful activation
     console.log('ServiceWorker activated successfully');
 });
 
+// Event listener for fetching resources
 self.addEventListener('fetch', function (e) {
     e.respondWith(
+         // Check if the requested resource is in the cache
         caches.match(e.request).then(function (response) {
+             // Return the cached response if available, otherwise fetch from the network
             return response || fetch(e.request);
         })
     );
